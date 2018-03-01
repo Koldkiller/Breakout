9df3b41047743e7829966ec4a0d402f4c28b2c8a
Sebastian <34139737+Koldkiller@users.noreply.github.com>
2018-03-01 13:02:46 +0100

    Update start.java

--- Breakout1/Model/start.java
+++ Breakout1/Model/start.java
@@ -16,8 +16,8 @@ public class start extends GraphicsProgram {
 	public static GRect paddle = null;
 	public static GOval ball = null;
 	public static int brick_counter = 0;
-	public Thread Move = new Thread(new Movement());
-	public Thread askCollision = new Thread(new Collision());
+	public static int level = 0;// rgen.nextInt(0, 2);
+	private static PlayGame game = new PlayGame();
 	public static final int WINDOW_WIDTH = 400;
 	public static final int WINDOW_HEIGHT = 800;
 	public static final int PADDLE_WIDTH = 40;
@@ -39,91 +39,18 @@ public class start extends GraphicsProgram {
 
 	public void drawtheGame() {
 		// Both still wrong size
-		paddle = Graphics.drawPaddle((WINDOW_WIDTH / 2) - (PADDLE_WIDTH / 2),
-				WINDOW_HEIGHT - (WINDOW_HEIGHT / 5));
+		paddle = Graphics.drawPaddle((WINDOW_WIDTH / 2) - (PADDLE_WIDTH / 2), WINDOW_HEIGHT - (WINDOW_HEIGHT / 5));
 		add(paddle);
 		ball = Graphics.drawBall((WINDOW_WIDTH / 2), WINDOW_HEIGHT - (WINDOW_HEIGHT / 5) - BALL_RAD);
 		ball.setColor(new Color(250, 0, 0));
 		add(ball);
-		int level = 0;// rgen.nextInt(0, 2);
-		switch (level) {
-		case (0):
-			// First Level
-			for (int i = 0; i < 10; i++) {
-				for (int j = 0; j < 11; j++) {
-					if (j % 3 == 0) {
-					} else {
-
-						int y = 20 + j * BRICK_HEIGHT;
-						int x = i * BRICK_WIDTH;
-						GRect ziegel = Graphics.drawBrick(x, y);
-
-						ziegel.setFillColor(new Color(0, 255 / (j + 1), 0));
-						add(ziegel);
-						brick_counter++;
-
-					}
-				}
-
-			}
-			break;
-
-		case (1):
-			// Map 2
-			// Looks ok, needs Color though
-			int j = 0;
-			for (int i = 0; i < 5; i++) {
-
-				int x = BRICK_WIDTH * (i + 2);
-				int y = BRICK_HEIGHT * j;
-				int x1 = BRICK_WIDTH * (6 - i);
-				GRect ziegel1 = Graphics.drawBrick(x, y);
-				GRect ziegel11 = Graphics.drawBrick(x1, y);
-				++j;
-				y = BRICK_HEIGHT * j;
-				GRect ziegel2 = Graphics.drawBrick(x, y);
-				GRect ziegel21 = Graphics.drawBrick(x1, y);
-				add(ziegel1);
-				add(ziegel2);
-				add(ziegel11);
-				add(ziegel21);
-				brick_counter = brick_counter + 4;
-				j++;
-			}
-			int k = 10;
-			for (int l = 0; l < 5; l++) {
-				int x = BRICK_WIDTH * (l + 2);
-				int y = BRICK_HEIGHT * k;
-				int x1 = BRICK_WIDTH * (6 - l);
-				GRect ziegel4 = Graphics.drawBrick(x, y);
-				GRect ziegel41 = Graphics.drawBrick(x1, y);
-				++k;
-				y = BRICK_HEIGHT * k;
-				GRect ziegel5 = Graphics.drawBrick(x, y);
-				GRect ziegel51 = Graphics.drawBrick(x1, y);
-				add(ziegel4);
-				add(ziegel41);
-				add(ziegel5);
-				add(ziegel51);
-				brick_counter = brick_counter + 4;
-				k++;
-
-			}
-			break;
-
-		case (2):
-			// Map 3
-
-		default:
-			throw new IllegalArgumentException("Something went srsly wrong with the Randomgenerator");
-		}
 
 	}
 
 	public void play() {
 		waitForClick();
 		while (true) {
-			PlayGame.update();
+			game.update();
 			askforkey.keyTyped(null);
 			Movement e = new Movement();
 			e.movementBall();
@@ -149,6 +76,81 @@ public class start extends GraphicsProgram {
 		IODialog gameOver = new IODialog();
 		ball.setVisible(false);
 		gameOver.print("You Lost!");
+	}
+
+	public static void drawBricks() {
+		// TODO Auto-generated method stub
+		switch (level) {
+		case (0):
+			// First Level
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < 11; j++) {
+					if (j % 3 == 0) {
+					} else {
+
+						int y = 20 + j * BRICK_HEIGHT;
+						int x = i * BRICK_WIDTH;
+						GRect ziegel = Graphics.drawBrick(x, y);
+
+						ziegel.setFillColor(new Color(0, 255 / (j + 1), 0));
+						game.addToScreen(ziegel);
+						brick_counter++;
+
+					}
+				}
+
+			}
+			break;
+
+		case (1):
+			// Map 2
+			// Looks ok, needs Color though
+			int j = 0;
+			for (int i = 0; i < 5; i++) {
+
+				int x = BRICK_WIDTH * (i + 2);
+				int y = BRICK_HEIGHT * j;
+				int x1 = BRICK_WIDTH * (6 - i);
+				GRect ziegel1 = Graphics.drawBrick(x, y);
+				GRect ziegel11 = Graphics.drawBrick(x1, y);
+				++j;
+				y = BRICK_HEIGHT * j;
+				GRect ziegel2 = Graphics.drawBrick(x, y);
+				GRect ziegel21 = Graphics.drawBrick(x1, y);
+				game.addToScreen(ziegel1);
+				game.addToScreen(ziegel2);
+				game.addToScreen(ziegel11);
+				game.addToScreen(ziegel21);
+				brick_counter = brick_counter + 4;
+				j++;
+			}
+			int k = 10;
+			for (int l = 0; l < 5; l++) {
+				int x = BRICK_WIDTH * (l + 2);
+				int y = BRICK_HEIGHT * k;
+				int x1 = BRICK_WIDTH * (6 - l);
+				GRect ziegel4 = Graphics.drawBrick(x, y);
+				GRect ziegel41 = Graphics.drawBrick(x1, y);
+				++k;
+				y = BRICK_HEIGHT * k;
+				GRect ziegel5 = Graphics.drawBrick(x, y);
+				GRect ziegel51 = Graphics.drawBrick(x1, y);
+				game.addToScreen(ziegel4);
+				game.addToScreen(ziegel41);
+				game.addToScreen(ziegel5);
+				game.addToScreen(ziegel51);
+				brick_counter = brick_counter + 4;
+				k++;
+
+			}
+			break;
+
+		case (2):
+			// Map 3
+
+		default:
+			throw new IllegalArgumentException("Something went srsly wrong with the Randomgenerator");
+		}
 	}
 
 }
