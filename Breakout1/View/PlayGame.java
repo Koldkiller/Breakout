3b072cc4c3edec3c936669a51649406804892829
Sebastian <34139737+Koldkiller@users.noreply.github.com>
2018-03-01 12:52:12 +0100

    update

--- Breakout1/View/PlayGame.java
+++ Breakout1/View/PlayGame.java
@@ -1,23 +1,49 @@
 package View;
 
+import java.awt.List;
+
+import Model.Movement;
+import acm.graphics.GObject;
 import acm.program.GraphicsProgram;
 
 public class PlayGame extends GraphicsProgram {
-	public static boolean running = false;
+	long last_time = System.nanoTime();
+	public boolean running = false;
+	private GObject oldball = null;
+	private GObject oldpaddle = null;
+	public List bricksToDelete = new List();
 
 	public PlayGame() {
-		running = true;
-		update();
+		
 	}
+	public void update() {
+		
 
-	public static void update() {
-		long vtime = System.nanoTime(); // nano second to milli second
-
-		while (running) {
-			long time = System.nanoTime();
-			int delta_time = (int) ((time - vtime) / 1000000);
-			vtime = time;
+		while(running) {
+		    long time = System.nanoTime();
+		    int delta_time = (int) ((time - last_time) / 1000000);
+		    
+		    //Moving the ball
+		    GObject newball = start.ball;
+		    addToScreen(newball);
+		    remove(oldball);
+		    oldball = newball;
+		    //Moving the paddle
+		    GObject newPaddle = start.paddle;
+		    addToScreen(newPaddle);
+		    remove(oldpaddle);
+		    oldball = newball;
+		    
+		    start.drawBricks()
+		    
+		    
+		    last_time = time;
 		}
+			 
+			 
+		 
 	}
-
+	public void addToScreen(GObject object) {
+		add(object);
+	}
 }
